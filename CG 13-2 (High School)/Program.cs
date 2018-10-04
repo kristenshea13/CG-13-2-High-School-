aaf85df46ddd05682a0633494201d5e0d0d7f875
Kristen Shea <kstar61@live.com>
2018-10-03 22:18:45 -0500

    Add project files.

---
+++ CG 13-2 (High School)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_13_2__High_School_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
