4ddc4865cc3bbf6f86e03df64ca6cbffb2d58b8f
Kristen Shea <kstar61@live.com>
2018-10-03 23:04:53 -0500

    updated comments

--- CG 13-2 (High School)/Program.cs
+++ CG 13-2 (High School)/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CG_13_2__High_School_
 {
@@ -6,7 +8,45 @@ namespace CG_13_2__High_School_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            //created list to keep all students of student class in
+            List<Student> students = new List<Student>
+            {
+                new Student() {FirstName = "Kristen", LastName = "Shea", Grade = 11, GPA = 3.7},
+                new Student() {FirstName = "Shannon", LastName = "Wrobleski", Grade = 7, GPA = 2.5},
+                new Student() {FirstName = "Stan", LastName = "Schoeber", Grade = 4, GPA = 4.0},
+                new Student() {FirstName = "David", LastName = "Hill", Grade = 2, GPA = 2.0},
+                new Student() {FirstName = "Katie", LastName = "Reifsteck", Grade = 7, GPA = 3.4},
+                new Student() {FirstName = "Karen", LastName = "Voss", Grade = 12, GPA = 1.0},
+                new Student() {FirstName = "Bill", LastName = "Shea", Grade = 9, GPA = 3.5},
+                new Student() {FirstName = "Emily", LastName = "Voss", Grade = 10, GPA = 3.0},
+                new Student() {FirstName = "Tanya", LastName = "Falls", Grade = 9, GPA = 1.7},
+                new Student() {FirstName = "Lindsay", LastName = "Webb", Grade = 3, GPA = 4.0},
+                new Student() {FirstName = "Molli", LastName = "Benz", Grade = 8, GPA = 3.5},
+                new Student() {FirstName = "William", LastName = "Vierling", Grade = 9, GPA = 3.1}
+            };
+
+            //created list to keep all students with a GPA of 3.0 or greater and in grade 9 or higher
+            //sorted students in list by grade first, then GPA, then last name, then first name
+            var highGPA = students.Where(s => s.GPA >= 3 && s.Grade >= 9 ).
+                OrderBy(s => s.Grade).OrderBy(s => s.GPA).OrderBy(s => s.LastName).
+                OrderBy(s => s.FirstName).ToList();
+
+            //printed student info from highGPA list to console, formatted for legibility
+            foreach (var student in highGPA)
+            {
+                Console.WriteLine("Name: {0} {1} \n Grade: {2} \n GPA: {3}", 
+                    student.FirstName, student.LastName, student.Grade, student.GPA);
+
+                Console.WriteLine();
+            }
+
+            
+            Console.ReadLine();
+
+           
+
         }
     }
 }
---
+++ CG 13-2 (High School)/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CG_13_2__High_School_
+{
+    class Student
+    {
+        //declared properties for student class
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Grade { get; set; }
+        public double GPA { get; set; }
+
+    }
+}
